1f7a39fefbbefdc6779a8f854a5f4a5a1846e556
rik0612c <qq872638399@gmail.com>
2023-08-11 22:50:05 +0800

    kenzo: Kang ld.config.txt

--- device.mk
+++ device.mk
@@ -84,6 +84,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
+# APEX
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/ld.config.txt:system/etc/swcodec/ld.config.txt
+
 # Doze mode
 PRODUCT_PACKAGES += \
     CustomDoze
